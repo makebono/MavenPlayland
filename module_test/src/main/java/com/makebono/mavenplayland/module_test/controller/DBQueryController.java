fd4a402f72eaf654c659e7a794767e313b76f1e0
makebono <xujiant@sfu.ca>
2017-12-01 11:39:16 +0800

    @Autowired worked.

--- module_test/src/main/java/com/makebono/mavenplayland/module_test/controller/DBQueryController.java
+++ module_test/src/main/java/com/makebono/mavenplayland/module_test/controller/DBQueryController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,9 +25,8 @@ import com.makebono.mavenplayland.module_test.service.QueryService;
 public class DBQueryController {
     private static final Logger logger = LoggerFactory.getLogger(DBQueryController.class);
 
-    // Try get the @Autowired working.
-    // @Autowired
-    private final QueryService sqlService = new QueryService();
+    @Autowired
+    private QueryService sqlService;
 
     @RequestMapping(value = "/queryById", method = { RequestMethod.POST, RequestMethod.GET })
     @ResponseBody
