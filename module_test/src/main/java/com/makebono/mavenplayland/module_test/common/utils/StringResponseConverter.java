bebbbd4fdc3b828be278fdc49a79c0696892bbd8
makebono <xujiant@sfu.ca>
2018-01-04 15:35:59 +0800

    Comments added. Structure adjusted and bug fixed.

---
+++ module_test/src/main/java/com/makebono/mavenplayland/module_test/common/utils/StringResponseConverter.java
@@ -0,0 +1,10 @@
+package com.makebono.mavenplayland.module_test.common.utils;
+
+/** 
+ * @ClassName: StringResponseConverter 
+ * @Description: Converter for @ResponseBody. Accepts String as output and print it the way Http be capable to deal with. 
+ * @author makebono
+ * @String 2018年1月4日 上午10:12:03 
+ *  
+ */
+public class StringResponseConverter extends CustomizedResponseConverter<String> {}
