31f9cbfa4bef829a66a474c92b7282ed42757309
makebono <xujiant@sfu.ca>
2018-01-04 15:38:27 +0800

    gbjj

---
+++ module_test/src/main/java/com/makebono/mavenplayland/module_test/common/utils/StudentResponseConverter.java
@@ -0,0 +1,12 @@
+package com.makebono.mavenplayland.module_test.common.utils;
+
+import com.makebono.mavenplayland.module_test.module.entities.Student;
+
+/** 
+ * @ClassName: StudentResponseConverter 
+ * @Description: Converter for @ResponseBody. Accepts Student as output and print it the way Http be capable to deal with. 
+ * @author makebono
+ * @Student 2018年1月4日 上午10:12:03 
+ *  
+ */
+public class StudentResponseConverter extends CustomizedResponseConverter<Student> {}
